473b5858c1df3123b59bd72a3822945d3975e917
mckoon <mckoon@gmail.com>
2018-04-16 20:28:27 -0400

    Issue #58: Member Directory
    * Lists all channel members on the member page of a channel.

--- groupease-client/src/app/channel-member/channel-member-directory/channel-member-directory.component.spec.ts
+++ groupease-client/src/app/channel-member/channel-member-directory/channel-member-directory.component.spec.ts
@@ -1,14 +1,73 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChannelMemberDirectoryComponent } from './channel-member-directory.component';
+import { MatExpansionModule, MatIconModule, MatListModule } from '@angular/material';
+import { MemberService } from '../../core/member.service';
+import { Member } from '../../core/member';
+import { Channel } from '../../core/channel';
+import { User } from '../../core/user';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ChannelMemberDirectoryComponent', () => {
   let component: ChannelMemberDirectoryComponent;
   let fixture: ComponentFixture<ChannelMemberDirectoryComponent>;
+  let memberService: jasmine.SpyObj<MemberService>;
+  let channel: Channel;
+  let member: Member;
 
   beforeEach(async(() => {
+
+    channel = new Channel();
+    channel.id = 123;
+    channel.name = 'channel name';
+    channel.description = 'channel description';
+    channel.lastUpdatedOn = 1523198394721;
+
+    member = new Member();
+    member.id = 456;
+    member.channel = channel;
+    member.owner = false;
+    member.groupeaseUser = new User();
+
+    memberService = jasmine.createSpyObj(
+      'MemberService',
+      [
+        'listAllInChannel'
+      ]
+    );
+
     TestBed.configureTestingModule({
-      declarations: [ ChannelMemberDirectoryComponent ]
+      declarations: [
+        ChannelMemberDirectoryComponent
+      ],
+      providers: [
+        {
+          provide: MemberService,
+          useValue: memberService
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            parent: {
+              parent: {
+                data: Observable.of(
+                  {
+                    channel: channel
+                  }
+                )
+              }
+            }
+          }
+        }
+      ],
+      imports: [
+        NoopAnimationsModule,
+        MatExpansionModule,
+        MatIconModule,
+        MatListModule
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +75,12 @@ describe('ChannelMemberDirectoryComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ChannelMemberDirectoryComponent);
     component = fixture.componentInstance;
+    memberService.listAllInChannel.and.returnValue(Observable.of( [ member ] ));
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
--- groupease-client/src/app/channel-member/channel-member.module.ts
+++ groupease-client/src/app/channel-member/channel-member.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ChannelMemberRoutingModule } from './channel-member-routing.module';
 import { ChannelMemberDirectoryComponent } from './channel-member-directory/channel-member-directory.component';
+import { MatExpansionModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    ChannelMemberRoutingModule
+    ChannelMemberRoutingModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatListModule
   ],
   declarations: [ChannelMemberDirectoryComponent]
 })
